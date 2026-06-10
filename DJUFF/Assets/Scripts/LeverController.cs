0b350d09e17df086bd06d95f23f3e5ec0f37c1c7
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-06-10 20:56:01 -0300

    Lever Model

--- DJUFF/Assets/Scripts/LeverController.cs
+++ DJUFF/Assets/Scripts/LeverController.cs
@@ -6,7 +6,10 @@ using UnityEngine.Events;
 public class LeverController : MonoBehaviour
 {
     [Header("Variables")]
+    [SerializeField] private GameObject leverHandle;
     [SerializeField] private bool activated = false;
+    [SerializeField] private float activatedAngle = -30;
+    [SerializeField] private float deactivatedAngle = -150;
 
     [Header("Events")]
     [SerializeField] private UnityEvent LeverActivated;
@@ -28,7 +31,6 @@ public class LeverController : MonoBehaviour
 
     public void ToggleInteract() 
     {
-        print("a");
         activated = !activated;
         if (activated)
             ActivateEffect();
@@ -39,10 +41,18 @@ public class LeverController : MonoBehaviour
     private void ActivateEffect() 
     {
         LeverActivated.Invoke();
+        Quaternion rotation = Quaternion.Euler(activatedAngle,
+                                               leverHandle.transform.rotation.y,
+                                               leverHandle.transform.rotation.z);
+        leverHandle.transform.localRotation = rotation;
     }
 
     private void DeactivateEffect() 
     {
         LeverDeactivated.Invoke();
+        Quaternion rotation = Quaternion.Euler(deactivatedAngle,
+                                               leverHandle.transform.rotation.y,
+                                               leverHandle.transform.rotation.z); 
+        leverHandle.transform.localRotation = rotation;
     }
 }
